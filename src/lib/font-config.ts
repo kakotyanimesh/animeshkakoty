30bbbf39ecca9770fb99836ff3605a43d54c23a9
animesh <phb21013@tezu.ac.in>
2025-09-13 11:00:06 +0530

    portfolio done

---
+++ src/lib/font-config.ts
@@ -0,0 +1,45 @@
+import localFont from "next/font/local";
+import { Anton } from "next/font/google";
+import { Pangolin } from "next/font/google";
+
+export const anton = Anton({
+    subsets: ["latin"],
+    weight: "400",
+    display: "swap",
+    variable: "--anton",
+});
+
+export const UpRightFontOne = Pangolin({
+    subsets: ["latin"],
+    weight: "400",
+    display: "swap",
+    variable: "--uprightone",
+});
+
+export const baseFont = localFont({
+    src: [
+        {
+            path: "../fonts/WorkSans-Light.otf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../fonts/WorkSans-ExtraLight.otf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../fonts/WorkSans-Thin.otf",
+            weight: "300",
+            style: "normal",
+        },
+    ],
+    variable: "--base-font",
+    display: "swap",
+});
+
+export const dottedFont = localFont({
+    src: "../fonts/Array-Regular.otf",
+    display: "swap",
+    variable: "--dotted-font",
+});
